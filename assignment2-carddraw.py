3a8126b75fd29d8a9afd836cbd7d5f43dc7276a7
jaked <jakedaly1997@hotmail.com>
2025-02-18 19:46:58 +0000

    assignment 2

--- assignment2-carddraw.py
+++ assignment2-carddraw.py
@@ -1,2 +1,18 @@
 # assignment2-carddraw.py
 # Author: Jake Daly
+
+import requests
+
+
+shuffle_url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+response = requests.get(shuffle_url)
+data = response.json()
+
+deck_id = data['deck_id']
+
+draw_url = f"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count=5"
+response = requests.get(draw_url)
+cards = response.json()['cards']
+
+for card in cards:
+    print("Card:", card['value'], "of", card['suit'])
